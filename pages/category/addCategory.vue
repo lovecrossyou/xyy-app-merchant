1efc11905354d7c264460e787376a365be4e5248
xiannvrong <1796940658@qq.com>
2019-03-19 10:41:33 +0800

    添加分类

---
+++ pages/category/addCategory.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="add_category_wrapper">
+		<view class="add_area">
+			<view class="name_text">名称</view>
+			<input type="text" value="" placeholder="请填写分类名称" />
+		</view>
+		<view class="add_area">
+			<view class="name_text">排序</view>
+			<input type="text" value="" placeholder="请填写数字(选填)" />
+		</view>
+		<button type="primary">完成</button>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	components: {},
+	computed: {},
+	methods: {}
+};
+</script>
+
+<style scoped>
+.add_category_wrapper {
+	width: 100%;
+	padding: 25upx;
+	box-sizing: border-box;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+}
+.add_area {
+	width: 100%;
+	height: 70upx;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+}
+.name_text {
+	color: #555555;
+	font-size: 28upx;
+	width: 15%;
+}
+.add_area > input {
+	flex: 1;
+	font-size: 24upx;
+	color: #999999;
+}
+.add_category_wrapper>button{
+	width:94%;
+	height: 88upx;
+	background: rgba(255, 134, 56, 1);
+	border-radius: 6px;
+	text-align: center;
+	line-height: 88upx;
+	color: #fff;
+	font-size: 28upx;
+	position: absolute;
+	bottom: 30upx;
+}
+</style>
--- pages/category/components/categoryListTemplate.vue
+++ pages/category/components/categoryListTemplate.vue
@@ -1,49 +1,52 @@
 <template>
 	<view class="wrapper">
-		<view class="title_text">{{title}}</view>
-		<image src="../../static/category/bianji@2x.png" mode="" class="change_img_icon"></image>
+		<view class="title_text">{{ title }}</view>
+		<image
+			src="../../static/category/bianji@2x.png"
+			mode=""
+			class="change_img_icon"
+			@click="addCategory"
+		></image>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		props:{
-			title:String
-		},
-		components: {
-			
-		},
-		computed: {
-			
-		},
-		methods: {
-			
+export default {
+	data() {
+		return {};
+	},
+	props: {
+		title: String
+	},
+	components: {},
+	computed: {},
+	methods: {
+		addCategory() {
+			uni.navigateTo({
+				url: '../category/addCategory'
+			});
 		}
 	}
+};
 </script>
 
 <style scoped>
-	.wrapper{
-		width: 100%;
-		height: 90upx;
-		background-color: #fff;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		align-items: center;
-		border-bottom: 1upx solid #EBEBEB;
-	}
-	.change_img_icon{
-		width: 24upx;
-		height: 28upx;
-	}
-	.title_text{
-		color: #FF8638;
-		font-size: 28upx;
-	}
+.wrapper {
+	width: 100%;
+	height: 90upx;
+	background-color: #fff;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	border-bottom: 1upx solid #ebebeb;
+}
+.change_img_icon {
+	width: 24upx;
+	height: 28upx;
+}
+.title_text {
+	color: #ff8638;
+	font-size: 28upx;
+}
 </style>
