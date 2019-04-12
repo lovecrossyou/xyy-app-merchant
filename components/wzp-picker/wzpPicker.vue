000d77e7bc0e2cca266116e842d00ad2f9e95907
love1234 <345841634@qq.com>
2019-04-12 16:49:31 +0800

    xxx

---
+++ components/wzp-picker/wzpPicker.vue
@@ -0,0 +1,244 @@
+<template>
+	<view style="width: 100%;">
+		<view class="picker-mask" @click="closePicker" catchtouchmove="true" v-show="pickerShow"></view>
+		<view class="picker-content" :class="{'pickerShow':pickerShow}">
+			<view class="picker-button">
+				<text @click="closePicker">取消</text>
+				<text @click="confirm">确定</text>
+			</view>
+			<!-- 单列选择 -->
+			<picker-view class="picker-view" indicator-class="picker-view-selected-one" :value="pickerIndex" @change="pickerViewChangeOne"
+			 v-if="mode=='one'">
+				<picker-view-column>
+					<view class="picker-item" v-for="(item, index) in pickerList" :key="index">{{item.label}}</view>
+				</picker-view-column>
+			</picker-view>
+			<!-- 双列选择-联动 -->
+			<picker-view class="picker-view" indicator-class="picker-view-selected-two" :value="pickerIndex" @change="pickerViewChangeTwo"
+			 v-if="mode=='two_linkage'">
+				<picker-view-column>
+					<view class="picker-item" v-for="(item, index) in pickerList" :key="index">{{item.label}}</view>
+				</picker-view-column>
+				<picker-view-column>
+					<view class="picker-item" v-for="(item, index) in pickerList[pickerIndex[0]].children" :key="index">{{item.label}}</view>
+				</picker-view-column>
+			</picker-view>
+			<!-- 双列选择-不联动 -->
+			<picker-view class="picker-view" indicator-class="picker-view-selected-two" :value="pickerIndex" @change="pickerViewChangeTwo"
+			 v-if="mode=='two_nolinkage'">
+				<picker-view-column>
+					<view class="picker-item" v-for="(item, index) in pickerList.firstArray" :key="index">{{item.label}}</view>
+				</picker-view-column>
+				<picker-view-column>
+					<view class="picker-item" v-for="(item, index) in pickerList.secondArray" :key="index">{{item.label}}</view>
+				</picker-view-column>
+			</picker-view>
+			<!-- 三列选择-联动 -->
+			<picker-view class="picker-view" indicator-class="picker-view-selected-three" :value="pickerIndex" @change="pickerViewChangeThree"
+			 v-if="mode=='three'">
+				<picker-view-column>
+					<view class="picker-item" v-for="(item, index) in pickerList" :key="index">{{item.label}}</view>
+				</picker-view-column>
+				<picker-view-column>
+					<view class="picker-item" v-for="(item, index) in pickerList[pickerIndex[0]].children" :key="index">{{item.label}}</view>
+				</picker-view-column>
+				<picker-view-column>
+					<view class="picker-item" v-for="(item, index) in pickerList[pickerIndex[0]].children[pickerIndex[1]].children"
+					 :key="index">{{item.label}}</view>
+				</picker-view-column>
+			</picker-view>
+		</view>
+	</view>
+</template>
+
+<script>
+	// mode所有类型
+	let pickerModeArray = ["one", "two_linkage", "two_nolinkage", "three"];
+
+	export default {
+		name: "wzp-picker",
+		props: {
+			// piker组件模式，默认单列
+			mode: {
+				type: String,
+				required: true,
+				// 校验数据
+				validator: (value) => {
+					return pickerModeArray.includes(value);
+				}
+			},
+			// picker组件数组
+			pickerList: {
+				type: Array,
+				required: true,
+			},
+			// 默认picker选中项索引
+			defaultIndex: {
+				type: Array,
+				required: true,
+				validator: (value) => {
+					if (value.length > 0 && value.length <= 3) {
+						return true;
+					}
+					return false;
+				}
+			}
+		},
+		data() {
+			return {
+				pickerIndex: [], // picker索引值
+				pickerShow: false
+			};
+		},
+		watch: {
+			// 匹配选中索引
+			mode() {
+				this.pickerIndex = this.defaultIndex;
+			}
+		},
+		methods: {
+			// 单列选项变化
+			pickerViewChangeOne(e) {
+				this.pickerIndex = e.detail.value;
+			},
+			// 双列选项变化-包含联动和不联动
+			pickerViewChangeTwo(e) {
+				let changeValue = e.detail.value;
+				if (this.mode == "two_linkage") {
+					// 超规处理
+					if (this.pickerList[changeValue[0]].children.length - 1 < changeValue[1]) {
+						changeValue[1] = this.pickerList[changeValue[0]].children.length - 1;
+					}
+				}
+				this.pickerIndex = changeValue;
+			},
+			// 三列联动选项变化
+			pickerViewChangeThree(e) {
+				let changeValue = e.detail.value;
+				// 超规处理
+				if (this.pickerList[changeValue[0]].children.length - 1 < changeValue[1]) {
+					changeValue[1] = this.pickerList[changeValue[0]].children.length - 1;
+				}
+				if (this.pickerList[changeValue[0]].children[changeValue[1]].children.length - 1 < changeValue[2]) {
+					changeValue[2] = this.pickerList[changeValue[0]].children[changeValue[1]].children.length - 1;
+				}
+				this.pickerIndex = changeValue;
+			},
+			// 显示组件
+			showPicker() {
+				// 隐藏软件盘
+				uni.hideKeyboard();
+				this.pickerShow = true;
+			},
+			// 确定事件——返回选中项的数组索引（也可以自定义其他返回数据，不过返回索引通用性更强）
+			confirm() {
+				this.pickerShow = false;
+				this.$emit("onConfirm", this.pickerIndex);
+			},
+			// 隐藏组件
+			closePicker() {
+				this.pickerShow = false;
+			}
+		}
+	}
+</script>
+
+<style>
+	.picker-mask {
+		position: fixed;
+		top: 0;
+		right: 0;
+		left: 0;
+		bottom: 0;
+		z-index: 50;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.3);
+	}
+
+	.picker-content {
+		flex-direction: column;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		z-index: 100;
+		width: 100%;
+		height: 600upx;
+		background-color: #FFFFFF;
+		transform: translateY(100%);
+		transition: all 200ms ease;
+	}
+
+	.pickerShow {
+		transform: translateY(0) !important;
+	}
+
+	.picker-content .picker-button {
+		width: 100%;
+		justify-content: space-between;
+		height: 80upx;
+		line-height: 80upx;
+		
+		flex-direction:row;
+		display:flex;
+
+	}
+
+	.picker-button text {
+		width: 180upx;
+		font-size: 28upx;
+		font-weight: 500;
+		display: block;
+		text-align: center;
+	}
+
+	.picker-button text:first-child {
+		color: #A1A1A1;
+	}
+
+	.picker-button text:last-child {
+		color: #1AAD19;
+	}
+
+	.picker-content .picker-view {
+		width: 100%;
+		height: 500upx;
+	}
+
+	.picker-content .picker-view-selected-one,
+	.picker-content .picker-view-selected-two,
+	.picker-content .picker-view-selected-three {
+		height: 100upx;
+		line-height: 100upx;
+		border-top: #1AAD19 1upx solid;
+		border-bottom: #1AAD19 1upx solid;
+	}
+
+	.picker-content .picker-view-selected-one {
+		position: relative;
+		left: 25%;
+		width: 50%;
+	}
+
+	.picker-content .picker-view-selected-two {
+		position: relative;
+		left: 15%;
+		width: 70%;
+	}
+
+	.picker-content .picker-view-selected-three {
+		position: relative;
+		left: 5%;
+		width: 90%;
+	}
+
+	.picker-view .picker-item {
+		width: 100%;
+		height: 100upx;
+		line-height: 100upx;
+		font-size: 28upx;
+		font-weight: 600;
+		display: block;
+		text-align: center;
+	}
+</style>
--- pages/shop/storeApply.vue
+++ pages/shop/storeApply.vue
@@ -6,22 +6,24 @@
 			<view style="width: 100%;padding:0 30upx;box-sizing: border-box;">
 				<view class="input_cont">
 					<view class="left_text">门店信息</view>
-					<input type="text" value="" placeholder="请与门脸照上名称一致" />
+					<input type="text" v-model="formData.name" :value="formData.name" placeholder="请与门脸照上名称一致" />
 				</view>
 				<view class="input_cont">
 					<view class="left_text">联系人电话</view>
-					<input type="text" value="" placeholder="请填写手机号" />
+					<input type="text" v-model="formData.phone" :value="formData.phone" placeholder="请填写手机号" />
 				</view>
 				<view class="input_cont">
 					<view class="left_text">门店分类</view>
-					<view class="center_text">请选择</view>
+					<view class="center_text" @click="showCategoryPicker">{{formData.category}}</view>
 					<image src="../../static/shop/xiala@2x.png" mode="" class="next_icon"></image>
 				</view>
-				<view class="input_cont">
+				
+				<view class="input_cont" @click="searchAddress">
 					<view class="left_text">门店地址</view>
-					<view class="center_text">请填写门店详细地址</view>
+					<view class="center_text">请选择地址</view>
 					<image src="../../static/shop/shouhuodizhi@2x.png" mode="" class="address_icon"></image>
 				</view>
+				
 			</view>
 		</view>
 		<!-- 门店照片 -->
@@ -43,109 +45,187 @@
 				<view class="right_explain">上传店铺logo,提升进店概率（ 支持JPG，JPEG，PNG格式， 大小不超过500K）</view>
 			</view>
 		</view>
+		<wzp-picker ref="wzpPicker" :mode="mode" :pickerList="pickerList" :defaultIndex="defaultIndex" :equalModeId="equalModeId"
+		 @onConfirm="onConfirm"></wzp-picker>
 		<button type="primary" class="submit_btn">提交</button>
 	</view>
 </template>
 
 <script>
-export default {
-	data() {
-		return {};
-	},
-	components: {},
-	computed: {},
-	methods: {}
-};
+	import wzpPicker from "../../components/wzp-picker/wzpPicker.vue";
+	import cityData from "../../common/city.data.js";
+	import province from '../../common/province.js';
+	export default {
+		data() {
+			return {
+				pickerValue: "",
+				mode: "",
+				pickerList: [],
+				defaultIndex: [],
+				addressDetail:'',
+				formData: {
+					"name": "邻家便利店",
+					"address": "北京市西城区百万庄大街11号",
+					"latitude": 39.92843,
+					"longitude": 116.35073,
+					"description": "快倒闭了",
+					"phone": 13220168837,
+					"promotion_info": "哈哈,就是便宜!",
+					"float_delivery_fee": 5,
+					"float_minimum_order_amount": 20,
+					"is_premium": true,
+					"delivery_mode": true,
+					"new": true,
+					"bao": true,
+					"zhun": true,
+					"piao": true,
+					"startTime": "05:30",
+					"endTime": "18:30",
+					"image_path": "http://static.kuaimayoupin.com/16a1076af23.jpg",
+					"business_license_image": "http://static.kuaimayoupin.com/16a1076b844.jpg",
+					"catering_service_license_image": "http://static.kuaimayoupin.com/16a1076cdd6.jpg",
+					"activities": [{
+						"icon_name": "减",
+						"name": "满减优惠",
+						"description": "满30减5，满60减8"
+					}],
+					"category": "请选择"
+				}
+			};
+		},
+		methods: {
+			searchAddress(){
+				uni.navigateTo({
+					url: '/pages/address/searchAddress'
+				})
+			},
+			// 双列联动初始化
+			showCategoryPicker() {
+				this.mode = 'two_linkage';
+				this.pickerList = cityData;
+				this.defaultIndex = [0, 0];
+				this.$refs.wzpPicker.showPicker();
+			},
+
+			onConfirm(e) {
+				const pickerValue = cityData[e[0]].label + "/" + cityData[e[0]].children[e[1]].label;
+				this.formData.category = pickerValue;
+			}
+		},
+		onBackPress() {
+			if (this.$refs.wzpPicker.pickerShow) {
+				this.$refs.wzpPicker.closePicker();
+				return true;
+			}
+		},
+		components: {
+			wzpPicker
+		}
+	};
 </script>
 
 <style scoped>
-.apply_wrapper {
-	width: 100%;
-}
-.basic_msg_text {
-	width: 100%;
-	height: 60upx;
-	background-color: #e6f1ff;
-	padding-left: 30upx;
-	box-sizing: border-box;
-	line-height: 60upx;
-	font-size: 24upx;
-	color: #74b1ff;
-}
-.input_cont {
-	width: 100%;
-	height: 95upx;
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	border-top: 1upx solid #e0e0e0;
-	padding: 0 10upx;
-	box-sizing: border-box;
-}
-.left_text {
-	width: 215upx;
-	text-align: left;
-	color: #333333;
-	font-size: 28upx;
-	font-weight: bold;
-}
-.input_cont > input {
-	flex: 1;
-	font-size: 26upx;
-}
-.center_text {
-	font-size: 26upx;
-	color: #888888;
-	flex: 1;
-}
-.next_icon {
-	width: 14upx;
-	height: 24upx;
-}
-.address_icon {
-	width: 32upx;
-	height: 40upx;
-}
-.input_cont:first-child {
-	border: none;
-}
-.store_imgs {
-	width: 100%;
-	padding: 0 33upx;
-	box-sizing: border-box;
-}
-.uploading_img_item {
-	width: 100%;
-	padding: 30upx 0;
-	display: flex;
-	flex-direction: row;
-	border-top: 1upx solid #e0e0e0;
-}
-.left_title {
-	color: #333333;
-	font-size: 24upx;
-	margin-top: 20upx;
-	width: 215upx;
-	font-weight: bold;
-}
-.add_img {
-	width: 110upx;
-	height: 110upx;
-}
-.right_explain {
-	width: 327upx;
-	margin-left: 28upx;
-	color: #888888;
-	font-size: 24upx;
-}
-.submit_btn {
-	width: 200upx;
-	height: 62upx;
-	background-color: #ff6b6b;
-	border-radius: 30upx;
-	text-align: center;
-	line-height: 62upx;
-	color: #fff;
-	margin-top: 60upx;
-}
+	.apply_wrapper {
+		width: 100%;
+	}
+
+	.basic_msg_text {
+		width: 100%;
+		height: 60upx;
+		background-color: #e6f1ff;
+		padding-left: 30upx;
+		box-sizing: border-box;
+		line-height: 60upx;
+		font-size: 24upx;
+		color: #74b1ff;
+	}
+
+	.input_cont {
+		width: 100%;
+		height: 95upx;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		border-top: 1upx solid #e0e0e0;
+		padding: 0 10upx;
+		box-sizing: border-box;
+	}
+
+	.left_text {
+		width: 215upx;
+		text-align: left;
+		color: #333333;
+		font-size: 28upx;
+		font-weight: bold;
+	}
+
+	.input_cont>input {
+		flex: 1;
+		font-size: 26upx;
+	}
+
+	.center_text {
+		font-size: 26upx;
+		color: #888888;
+		flex: 1;
+	}
+
+	.next_icon {
+		width: 14upx;
+		height: 24upx;
+	}
+
+	.address_icon {
+		width: 32upx;
+		height: 40upx;
+	}
+
+	.input_cont:first-child {
+		border: none;
+	}
+
+	.store_imgs {
+		width: 100%;
+		padding: 0 33upx;
+		box-sizing: border-box;
+	}
+
+	.uploading_img_item {
+		width: 100%;
+		padding: 30upx 0;
+		display: flex;
+		flex-direction: row;
+		border-top: 1upx solid #e0e0e0;
+	}
+
+	.left_title {
+		color: #333333;
+		font-size: 28upx;
+		margin-top: 20upx;
+		width: 215upx;
+		font-weight: bold;
+	}
+
+	.add_img {
+		width: 110upx;
+		height: 110upx;
+	}
+
+	.right_explain {
+		width: 327upx;
+		margin-left: 28upx;
+		color: #888888;
+		font-size: 24upx;
+	}
+
+	.submit_btn {
+		width: 200upx;
+		height: 62upx;
+		background-color: #ff6b6b;
+		border-radius: 30upx;
+		text-align: center;
+		line-height: 62upx;
+		color: #fff;
+		margin-top: 60upx;
+	}
 </style>
