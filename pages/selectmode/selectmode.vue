d0d68b5640b5393fe5250a35ce948f5a75b06272
love1234 <345841634@qq.com>
2019-06-11 17:41:47 +0800

    xx

---
+++ pages/selectmode/selectmode.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class="main">
+		<view class="header_bg">
+			<view class="main_text">包装水行业备案小程序</view>
+		</view>
+		
+		<view class="item">
+			水厂备案
+		</view>
+
+		<view class="item">
+			水站备案
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.main {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		/* justify-content: center; */
+		align-items: center;
+	}
+
+	.title {
+		font-size: 50upx;
+		color: #333333;
+		padding: 30upx 0upx;
+		margin-bottom: 120upx;
+	}
+
+	.header_bg {
+		width: 100%;
+		height: 400upx;
+		background: url(http://qnimage.xiteng.com/banner@2x.png) no-repeat center center;
+		background-size: 100% 100%;
+		
+		/* position: relative; */
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		
+		margin-bottom: 20upx;
+	}
+	
+	.main_text{
+		color: #999999;
+		font-size: 30upx;
+		
+	}
+
+	.item {
+		padding: 50upx 20upx;
+		text-align: center;
+		width: 100%;
+		box-sizing: border-box;
+		border: dashed 1upx #E1E1E1;
+		font-size: 40upx;
+		color: #333333;
+
+		margin-bottom: 20upx;
+	}
+</style>
